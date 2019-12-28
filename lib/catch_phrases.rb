815cb9f84b50f013f942c545eb9673735f24c1af
Adam Longman <boardhead89@gmail.com>
2019-12-28 17:45:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,16 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a-me, Mario!"
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  phrase = "It's dangerous to go alone! Take this."
 end
 
 def any_phrase
